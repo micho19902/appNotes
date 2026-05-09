ea39c1a81e8ae0920e2be9bb46571b44d7017424
micho1990 <micho.1990@gmail.com>
2026-05-09 19:52:47 -0400

    no sirve la fecha formateada

--- appNotes/model/modelNotes.py
+++ appNotes/model/modelNotes.py
@@ -1,10 +1,20 @@
 from sqlmodel import SQLModel, Field
 import datetime
 
+class User(SQLModel, table=True):
+    # __tablename__ = "users"
+    
+    id: int | None = Field(default=None, primary_key=True)
+    username: str = Field(unique=True, index=True, max_length=50)
+    email: str = Field(unique=True, index=True, max_length=100)
+    hashed_password: str
+    is_active: bool = True
+    # created_at: datetime = Field(default_factory=datetime.now)
+    # last_login: Optional[datetime] = None
 
 class Notes(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    user: str
+    user: int
     note: str
     date: datetime.datetime
     level: int
@@ -12,3 +22,4 @@ class Notes(SQLModel, table=True):
 class Level(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     level: str    
+
